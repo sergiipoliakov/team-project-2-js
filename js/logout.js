83a568d21d6ea394ea04defff3007424a3ae88f9
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-12-09 19:21:17 +0200

    стартовый вариянт готов

---
+++ js/logout.js
@@ -0,0 +1,17 @@
+console.log(555);
+const refs = {
+  openModalBtn: document.querySelector('[data-exit]'),
+  closeModalBtn: document.querySelector('[data-modal-logout-close]'),
+  backdrop: document.querySelector('[data-modal-logout]'),
+};
+
+console.log(refs.backdrop);
+console.log(refs.openModalBtn);
+console.log(refs.closeModalBtn);
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  refs.backdrop.classList.toggle('is-hidden');
+}
---
+++ js/search-model-form.js
@@ -0,0 +1,18 @@
+console.log(555);
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  backdrop: document.querySelector('[data-modal]'),
+};
+
+console.log(refs.backdrop);
+console.log(refs.openModalBtn);
+console.log(refs.closeModalBtn);
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  console.log('eqweqw');
+  refs.backdrop.classList.toggle('is-hidden');
+}
