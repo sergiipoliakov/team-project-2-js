b71f9f8f4877650d1b58156a8bbae23c5c690239
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-12-12 00:55:07 +0200

    регистрация закончена

--- src/js/header/login.js
+++ src/js/header/login.js
@@ -1,3 +1,7 @@
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import { error } from '@pnotify/core';
+
 const refs = {
   openModalRegisteBtn: document.querySelector('[data-register]'),
   closeModalBtn: document.querySelector('[data-modal-register-close]'),
@@ -10,6 +14,7 @@ const exitBtn = document.querySelector('.js-logout-button');
 const myStorage = window.localStorage;
 
 let token = myStorage.getItem('Bearer');
+console.log(token);
 
 if (token) {
   loginRegisterBtn.innerHTML = '';
@@ -27,7 +32,8 @@ function toggleModal() {
 const formInputEmail = document.querySelector('#e-mail');
 const formInputPassword = document.querySelector('#password');
 const formRegistration = document.querySelector('[data-register-form]');
-const registerFormBtn = document.querySelector('#login');
+const loginFormBtn = document.querySelector('#login');
+const registerFormBtn = document.querySelector('#register');
 
 let registerFormQuery = {};
 
@@ -35,22 +41,81 @@ formRegistration.addEventListener('submit', onRegisterFormSubmit);
 
 function onRegisterFormSubmit(event) {
   event.preventDefault();
-  toggleModal();
+  // toggleModal();
+
+  const currentClick = event.currentTarget.nodeName;
+  console.dir(currentClick);
 
   registerFormQuery.email = formInputEmail.value;
   registerFormQuery.password = formInputPassword.value;
 
-  resolt(registerFormQuery).then(data => {
-    const Token = data.accessToken;
+  if (currentClick.id === 'login') {
+    console.log('super');
+  }
+  toLogin(registerFormQuery)
+    .then(data => {
+      console.log(data.accessToken);
 
-    localStorage.setItem('Bearer', Token);
-    location.reload();
-  });
+      const message = data.message;
+      console.log(data.message);
+      if (message) {
+        return error({
+          text: message,
+          type: 'info',
+          animateSpeed: 'normal',
+          delay: 1000,
+        });
+      }
+
+      const Token = data.accessToken;
+
+      if (Token !== undefined) {
+        localStorage.setItem('Bearer', Token);
+        location.reload();
+      } else if (Token === undefined) {
+        toRegistation(registerFormQuery).then(data => {
+          console.log(data.accessToken);
+          const Token = data.accessToken;
+
+          if (Token !== undefined) {
+            localStorage.setItem('Bearer', Token);
+            location.reload();
+          }
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
-const resolt = function (data) {
+const toLogin = function (data) {
   async function postData(
     url = 'https://callboard-backend.herokuapp.com/auth/login',
+    data = registerFormQuery,
+    method = 'POST',
+  ) {
+    const response = await fetch(url, {
+      method: method, // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json',
+        // Authorization:
+        //   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmQzMzYxZjgwZGFiZDAwMTc5ZDdmZjYiLCJzaWQiOiI1ZmQzMzY0MjgwZGFiZDAwMTc5ZDdmZjkiLCJpYXQiOjE2MDc2Nzc1MDYsImV4cCI6MTYwNzY4MTEwNn0.RnvvG68q1yWWaIVr777cLMJg-eNwugnc7x5ldqFuoNM',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+
+    return await response.json();
+
+    // parses JSON response into native JavaScript objects
+  }
+  return postData();
+};
+const toRegistation = function (data) {
+  async function postData(
+    url = 'https://callboard-backend.herokuapp.com/auth/register',
     data = registerFormQuery,
     method = 'POST',
   ) {
