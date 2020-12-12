81908c4e80c289a43865e07a9c34fad2a7133866
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-12-12 22:50:51 +0200

    добавил поиск по категориям

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import './sass/main.scss';
 import 'regenerator-runtime/runtime';
+import './sass/main.scss';
 import './js/header/header-main';
 import './js/header/start-page/start-page';
 import './js/header/pagination';
+import './js/main/catigoriesApi';
