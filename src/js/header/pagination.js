576d1b4afbfec996057dd9d7d412f38038162cbc
Vitalii <65967741+SolonukhaVitalii@users.noreply.github.com>
2020-12-13 16:49:45 +0200

    pagination_js

--- src/js/header/pagination.js
+++ src/js/header/pagination.js
@@ -1,12 +1,31 @@
-// const refs = {
-//   numberBtn: document.querySelector(`.button`),
-// };
-
-// console.log(refs.numberBtn);
-
-// refs.numberBtn.addEventListener('click', onNumberBtn);
-
-// function onNumberBtn(e) {
-//   e.preventDefault();
-//   newApiService.fetchApiRegists().then(renderApiRegists).catch(onFetchError);
-// }
+import catigoriesTmpl from '../../templates/catigories.hbs';
+import { API } from './apiServer';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import { error } from '@pnotify/core';
+const categoriesListContainer = document.querySelector(`.js-categories-list`);
+const catogoriesContainer = document.querySelector(`.js-catigories`);
+let catigoriesQuery;
+categoriesListContainer.addEventListener('click', onCategoriesItem);
+function onCategoriesItem(e) {
+  e.preventDefault();
+  catigoriesQuery = e.target.id;
+  const url = `https://callboard-backend.herokuapp.com/call/specific/${catigoriesQuery}`;
+  toCatigoriesClick(url).then(data => {
+    catogoriesContainer.innerHTML = catigoriesTmpl(data);
+  });
+}
+const toCatigoriesClick = function (param) {
+  async function postData(url = param, method = 'GET') {
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+    });
+    return await response.json();
+  }
+  return postData();
+};
