da59d0fa14b44105c04935371e48c8ea22dcd4c0
Vitalii <65967741+SolonukhaVitalii@users.noreply.github.com>
2020-12-13 20:54:19 +0200

    pagination_js_2

--- src/js/header/pagination.js
+++ src/js/header/pagination.js
@@ -1,31 +1,24 @@
-// import catigoriesTmpl from '../../templates/catigories.hbs';
-// import { API } from './apiServer';
-// import '@pnotify/core/dist/BrightTheme.css';
-// import '@pnotify/core/dist/PNotify.css';
-// import { error } from '@pnotify/core';
-// const categoriesListContainer = document.querySelector(`.js-categories-list`);
-// const catogoriesContainer = document.querySelector(`.js-catigories`);
-// let catigoriesQuery;
-// categoriesListContainer.addEventListener('click', onCategoriesItem);
-// function onCategoriesItem(e) {
-//   e.preventDefault();
-//   catigoriesQuery = e.target.id;
-//   const url = `https://callboard-backend.herokuapp.com/call/specific/${catigoriesQuery}`;
-//   toCatigoriesClick(url).then(data => {
-//     catogoriesContainer.innerHTML = catigoriesTmpl(data);
-//   });
-// }
-// const toCatigoriesClick = function (param) {
-//   async function postData(url = param, method = 'GET') {
-//     const response = await fetch(url, {
-//       method: method,
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       redirect: 'follow',
-//       referrerPolicy: 'no-referrer',
-//     });
-//     return await response.json();
-//   }
-//   return postData();
-// };
+//import categoriesApi from '../main/catigoriesApi';
+
+//let page = 1;
+//const refs = {
+ //   numberBtn: document.querySelector(`.button`),
+//};
+
+//refs.numberBtn.addEventListener('click', onNumberBtn);
+
+//function onNumberBtn(e) {
+//  e.preventDefault();
+// categoriesApi.fetchArticles().then(appendArticlesMarkup);
+//if (page += 1) {
+//  categoriesApi();
+//}
+// if (page += 2) {
+//  categoriesApi();
+//}
+//};
+// function appendArticlesMarkup(articles) {
+//  refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+//}
+
+
