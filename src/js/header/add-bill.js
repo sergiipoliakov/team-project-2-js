5ec3dbd9601f810bcc123516c26d25833a74cce5
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-12-14 10:34:09 +0200

    форма подачи обьявления готов.html

--- src/js/header/add-bill.js
+++ src/js/header/add-bill.js
@@ -1,21 +1,44 @@
 // console.log('bill');
 const refs = {
   openModalBillBtn: document.querySelector('[data-add-bill]'),
-  //   openModalLoginBtn: document.querySelector('[data-login]'),
-
   closeModalBtn: document.querySelector('[data-modal-add-bill-close]'),
   backdrop: document.querySelector('[data-modal-add-bill]'),
 };
-
-// console.log(refs.backdrop);
-// console.log(refs.openModalBillBtn);
-// console.log(refs.closeModalBtn);
 
 refs.openModalBillBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 // refs.openModalLoginBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
-  // console.log('eqweqw');
   refs.backdrop.classList.toggle('is-hidden');
+}
+
+const addBillFormEl = document.querySelector('.js-bill-form');
+const nameBillInputEl = document.querySelector('#nameBill');
+const addFileInputEl = document.querySelector('#file');
+const addAboutBillInputEl = document.querySelector('#aboutBill');
+const addCategoryInputEl = document.querySelector('#category');
+const addPriceInputEl = document.querySelector('#price');
+const addTelefonInputEl = document.querySelector('#telefon');
+
+let formData = {};
+
+console.log(addBillFormEl);
+console.log(addFileInputEl);
+
+addBillFormEl.addEventListener('submit', takeFormData);
+
+//   const formData = Object.fromEntries(new FormData(e.target).entries());
+//   console.log(formData);
+// });
+function takeFormData(event) {
+  event.preventDefault();
+  formData.title = nameBillInputEl.value;
+  formData.file = addFileInputEl.value;
+  formData.description = addAboutBillInputEl.value;
+  formData.category = addCategoryInputEl.value;
+  formData.price = addPriceInputEl.value;
+  formData.phone = addTelefonInputEl.value;
+
+  console.log(formData);
 }
