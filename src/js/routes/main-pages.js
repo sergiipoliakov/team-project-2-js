c93f04663f609019db60df0ad52444ba71fff9b7
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-12-17 22:22:26 +0200

    int

---
+++ src/js/routes/main-pages.js
@@ -0,0 +1,16 @@
+// import { updateState, updatedContent } from './rout-main'
+
+// const listCategories = document.querySelector('.all-button');
+
+// const searchCategories = (e) => {
+//     const elem = e.target;
+//     if (!elem.classList.contains("main-btn")) return
+//     // console.log(elem.classList.contains("category-it"));
+//     e.preventDefault();
+//     let value = elem.getAttribute('data-attribute');
+//     updateState(`/page${value}`);
+//     updatedContent();
+//     console.log(value);
+// }
+
+// listCategories.addEventListener('click', searchCategories)
