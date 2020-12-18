f08f419a35fe9ce69724a8e8c3c32bb306b53ad8
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-12-18 23:30:07 +0200

    пофикси поиск

--- src/js/main/edit-modal.logic.js/open-edit-modal.js
+++ src/js/main/edit-modal.logic.js/open-edit-modal.js
@@ -1,14 +1,4 @@
-// const dataAtribut = document.querySelector('.product-card').getAttribute();
-// console.log(dataAtribut);
-
-{
-  /* <div class="getAtribut" data-atribu-12345></div>; */
-}
-
 const myCard = document.querySelector('.my-goods-hbs');
-
-console.log(myCard);
-// const card = document.querySelector('.getAtribut');
 
 myCard.addEventListener('click', getAtribut);
 
@@ -16,13 +6,8 @@ function getAtribut(e) {
   if (!e.target.nodeName === 'IMG') {
     return;
   }
-  // === 'IMG'
-
   const dataAtribut = document
     .querySelector('.product-card')
     .getAttribute('data-cart');
-  //   return dataAtribut;
-  console.log(dataAtribut);
+  return dataAtribut;
 }
-
-// const dataAtribut = card.dataset.set;
