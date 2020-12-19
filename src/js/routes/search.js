01a2f93862d8750d0b3865bf36b6d5ea48cc5e3e
Andrii-Velhan <velganaa@gmail.com>
2020-12-19 03:09:42 +0200

    add styles to cards adaptiv work ONLY to 1th container
    adaptiv work ONLY to 1th container

--- src/js/routes/search.js
+++ src/js/routes/search.js
@@ -4,11 +4,11 @@ let search = document.querySelector('#searchInput');
 let btn = document.querySelector('#btn');
 
 let searchProduct = (e) => {
-    e.preventDefault();
-    let value = search.value;
-    if (!value) return
-    updateState(`/search?value=${value}`);
-    updatedContent()
+	e.preventDefault();
+	let value = search.value;
+	if (!value) return
+	updateState(`/search?value=${value}`);
+	updatedContent()
 }
 
 btn.addEventListener('click', searchProduct)
